96c95140406aec864dd28abc8ebd69cd3dc12405
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-21 02:35:17 +0430

    Add person methods

--- person.rb
+++ person.rb
@@ -1,11 +1,22 @@
 class Person
     attr_reader :id, :name, :age
     attr_writer :name, :age
-    
+
     def initialize (name = "unknown" , age, parent_permission = true)
         @id = Random.rand(1..1000)
         @name = name
         @age = age
     end    
     
+    def is_of_age? 
+        @age >= 18 ? true : false
+    end
+
+    def can_use_services?
+        if is_of_age? || @parent_permission
+            return true
+        end
+    end
+
+    private :is_of_age?
 end
