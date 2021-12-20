ce3a3a280880bab6c5f4d1fdfda67fd61d013c55
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-21 02:52:20 +0430

    Add Teacher class and override can_use_services

--- teacher.rb
+++ teacher.rb
@@ -1,2 +1,12 @@
 require './person.rb'
 
+class Teacher
+    def initialize (age, specialization)
+        super(age)
+        @specialization = specialization
+    end
+    
+    def can_use_services?
+        true
+    end
+end
