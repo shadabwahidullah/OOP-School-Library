4579dd6f0f2783cf855e7236d6f77377ba81c4af
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-24 01:23:01 +0430

    Add validate name in teacher

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require './corrector'
 
 class Person
-  def initialize(age, name = 'unknown', parent_permission: true)
+  def initialize(age, name = 'unknown', parent_permission = true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
--- teacher.rb
+++ teacher.rb
@@ -1,12 +1,16 @@
 require './person'
+require './corrector'
 
 class Teacher < Person
-  def initialize(age, specialization, name = 'unknown', parent_permission: true)
+  def initialize(age, specialization, name = 'unknown', parent_permission = true)
     super(age, name, parent_permission)
     @specialization = specialization
+    @Corrector = Corrector.new
   end
 
   def can_use_services?
     true
   end
 end
+
+puts Teacher.new(17, 'something', 'wahidullah').validate_name
