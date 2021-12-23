872a6c93daf33d435c0da03207a0d52faa91cc31
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-24 01:30:44 +0430

    Remove corrector initialization from teacher initialize method

--- teacher.rb
+++ teacher.rb
@@ -5,7 +5,6 @@ class Teacher < Person
   def initialize(age, specialization, name = 'unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @specialization = specialization
-    @Corrector = Corrector.new
   end
 
   def can_use_services?
