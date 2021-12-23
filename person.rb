c9d98489fcddb2d4d111d3cda4dda5dfbe9a81f2
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-24 01:28:59 +0430

    Resolve linters errors

--- person.rb
+++ person.rb
@@ -9,7 +9,7 @@ class Person
     @corrector = Corrector.new
   end
   attr_accessor :name, :age
-  attr_reader :id 
+  attr_reader :id
 
   def can_use_services?
     true if of_age? || @parent_permission
