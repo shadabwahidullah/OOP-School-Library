f8e3b10742e86c962f22efd4ab7e467d9a60e8bd
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-24 15:59:16 +0430

    Create a rental belongs to person relationship

--- rental.rb
+++ rental.rb
@@ -1,5 +1,5 @@
 class Rental
-    attr_accessor :date, :book
+    attr_accessor :date, :book, :person
     
     def initialize (date)
         @date = date        
@@ -8,5 +8,10 @@ class Rental
     def book=(book)
         @book = book
         book.rental.push(self) unless book.rental.include?(self)
+    end
+
+    def  person=(person)
+        @person = person
+        person.rental.push(self) unless person.rental.include?(self)
     end
 end
