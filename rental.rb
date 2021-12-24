5120a83c71650da274487cb1f340a621689f84db
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-24 15:55:10 +0430

    Create a rental belongs to a book

--- rental.rb
+++ rental.rb
@@ -1,7 +1,12 @@
 class Rental
-    attr_accessor :date
+    attr_accessor :date, :book
     
     def initialize (date)
         @date = date        
+    end
+
+    def book=(book)
+        @book = book
+        book.rental.push(self) unless book.rental.include?(self)
     end
 end
