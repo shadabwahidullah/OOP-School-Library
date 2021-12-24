61f1c8b2a5f7be8520233c4379cf1ea1f2b911e1
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-24 20:28:59 +0430

    Created a bug in person

--- person.rb
+++ person.rb
@@ -4,7 +4,6 @@ class Person
   def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
-    @age = age
     @parent_permission = parent_permission
     @corrector = Corrector.new
   end
