6e893f630762f8f60d1d4c85cfc797dcb60f46c3
KatIsCoding <fabrigoru@gmail.com>
2022-01-05 13:12:34 -0600

    get_data_object method added to the ListClass

--- list_middleware.rb
+++ list_middleware.rb
@@ -1,8 +1,17 @@
 class ListClass
-  def initialize
-    @books = []
-    @rentals = []
-    @people = []
+  def initialize(data_object)
+    @books = data_object[:books]
+    @rentals = data_object[:rentals]
+    @people = data_object[:people]
   end
   attr_accessor :people, :books, :rentals
+
+  def get_data_object()
+    output = {
+      people: @people,
+      books: @books,
+      rentals: @rentals
+    }
+    return output
+  end
 end
