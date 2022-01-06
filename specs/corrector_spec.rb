c4bd3bb98b06222a9614aa83fb5fadb6ef6b65f7
KatIsCoding <fabrigoru@gmail.com>
2022-01-06 13:28:45 -0600

    Corrector testing

--- specs/corrector_spec.rb
+++ specs/corrector_spec.rb
@@ -9,4 +9,12 @@ describe Corrector do
             expect(@corrector).to be_a Corrector
         end
     end
+
+    context 'correct name' do
+        it 'try correct name' do
+            mock_name = "aaaaaaaaaaa"
+            corrected_name = @corrector.correct_name(mock_name)
+            expect(corrected_name).to eq "Aaaaaaaaaa"
+        end
+    end
 end
