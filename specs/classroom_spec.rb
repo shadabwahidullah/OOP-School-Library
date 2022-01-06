8dddf8f190d88e18eaf3f6529ff835413d53be34
shadabwahidullah <shadabwahidullah@gmail.com>
2022-01-06 23:55:29 +0430

    Add test for adding student to class

--- specs/classroom_spec.rb
+++ specs/classroom_spec.rb
@@ -1,4 +1,5 @@
 require_relative '../classroom'
+require_relative '../student'
 
 describe Classroom do
     before( :each) do
@@ -8,6 +9,14 @@ describe Classroom do
     context "Classroom instanitiation" do
         it "#new" do
             expect(@c).to be_a Classroom
+        end
+    end
+
+    context "Add student to class" do
+        it "add student" do
+            s = Student.new age:12, classroom: @c
+            @c.add_student(s)
+            expect(@c.students.length).to be(1)
         end
     end
 end
