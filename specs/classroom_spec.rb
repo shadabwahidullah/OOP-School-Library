28c100f2a569bc871dec6935b43acd62139532e0
shadabwahidullah <shadabwahidullah@gmail.com>
2022-01-07 02:10:51 +0430

    Update classroom_spec.rb

--- specs/classroom_spec.rb
+++ specs/classroom_spec.rb
@@ -6,7 +6,7 @@ describe Classroom do
     @c = Classroom.new 'label'
   end
 
-  context 'Classroom instanitiation' do
+  context 'Classroom instantiation' do
     it '#new' do
       expect(@c).to be_a Classroom
     end
